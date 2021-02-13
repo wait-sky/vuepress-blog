81cd3d2d5fb26959da4e1f6155da502be3230d66
ayil <123942353@qq.com>
2021-02-13 19:39:45 +0800

    增加.gitignore文件, 配置package.json

---
+++ docs/.vuepress/components/btn.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="btn">
+    <button @click="handleButtonClick">我是按钮</button>
+  </div>
+</template>
+
+<script>
+  export default {
+    methods: {
+      handleButtonClick () {
+        console.log('click..fdsa.')
+      }
+    }
+  }
+</script>
+
+<style scoped>
+.btn {
+  margin: 0;
+  padding: 0;
+}
+</style>
---
+++ docs/tech/README.md
@@ -0,0 +1,1 @@
+<btn>fdsa</btn>
