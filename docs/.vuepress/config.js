6dfd892eb00a1d40bdac149259ebd56c018fe279
ayil <123942353@qq.com>
2021-02-14 21:56:04 +0800

    字段说明调整

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -3,14 +3,15 @@ module.exports = {
   // base: 'manman', 分支名称, 如果是带有GitHub名称的就是默认/, 可以不写
   head: [
     // 注入到当前页面的 HTML <head> 中的标签
-    ["link", { rel: "icon", href: "/m.jpg" }], // 增加一个自定义的 favicon(网页标签的图标)
+    ["link", { rel: "icon", href: "/avatar.jpg" }], // 增加一个自定义的 favicon(网页标签的图标)
   ],
   plugins: ["@vuepress/back-to-top"], // 滚动到顶部插件 `yarn add -D @vuepress/plugin-back-to-top`
   themeConfig: {
     logo: "/avatar.jpg", // 左上角logo
     // 导航栏配置
     nav: [
-      { text: "首页", link: "/" },
+      // { text: "首页", link: "/" },
+      { text: "指南", link: "/guide/" },
       { text: "集成element", link: "http://xuedingmiao.com/blog/vuepress_element.html" },
       { text: "vue文件测试", link: "/tech/" },
       { text: "学习文档", link: "https://www.jianshu.com/p/37509da5a020" },
@@ -37,8 +38,9 @@ module.exports = {
         ],
       },
     ],
-    sidebar: "auto", // 侧边栏配置
+    // sidebar: "auto", // 侧边栏配置 不能深层次查找
     sidebarDepth: 2, // 二级标题
+    displayAllHeaders: true, // 默认值：false
     // 侧边栏配置
     sidebar: {
       "/REACT/": ["" /* /foo/ */, "re" /* /foo/one.html */],
--- docs/README.md
+++ docs/README.md
@@ -1,16 +1,20 @@
 ---
 home: true
 heroImage: /m.jpg
-# heroText: 我的主页
-tagline: My homepage
-actionText: 技术文档 →
-actionLink: /tech/interview/
+heroText: 新世界, 新征程
+tagline: New World, New Journey
+actionText: To Do What You Want →
+actionLink: /guide/
 features:
-- title: 技术
-  details: 以 Markdown 为中心的项目结构，以最少的配置帮助你专注于写作。
-- title: 生活
-  details: 享受 Vue + webpack 的开发体验，在 Markdown 中使用 Vue 组件，同时可以使用 Vue 来开发自定义主题。
-- title: 文学
-  details: VuePress 为每个页面预渲染生成静态的 HTML，同时在页面被加载的时候，将作为 SPA 运行。
-footer: 满满的个人博客
+- title: 技 术 成 长
+  details: 以掌握的技术为基础, 不断积累, 不断记录. 作为知识储备, 每一次新的尝试, 都是自我超越.
+- title: 奇 闻 异 事
+  details: 享受生活, 记录生活中的趣事, 以丰富陶冶自我情操, 让生活充满快乐的情绪.
+- title: 文 学 经 典
+  details: 记录好词, 好句, 好段, 好文. 经常读书是一种自我修养的提高与能力的提升. 
+- title: 情 感 360 °
+  details: 人物的情感撰写, 用以体会不同场景的换位思考与感同身受. 
+- title: 我 的 故 事
+  details: 自我的文学记录
+footer: 满满的个人博客 copyright@manman
 ---
